e71f0b8db8cd2c8f6150f96efe7bbdf48f05c6b2
ash15764 <136904070+ash15764@users.noreply.github.com>
2024-12-23 21:41:43 +0200

    Create TextFrameNode.jsx

---
+++ frontend/src/UMLComponents/TextFrameNode.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Textframe from './Components/textframe';
+
+const TextframeNode = ({ data }) => {
+  return (
+    <div style={{ padding: 10}}>
+      <Textframe />
+    </div>
+  );
+};
+
+export default TextframeNode;
